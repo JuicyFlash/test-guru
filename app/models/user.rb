bebd655170d4c20791a335c97f3a676eea849c35
JuicyFlash <tankedet@bk.ru>
2022-02-15 19:56:42 +0300

    метод get_tests_by_level возвращает список тестов пользователя по уровню сложности

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  def get_tests_by_level(level)
+    Test.joins("JOIN processed_tests on tests.id = processed_tests.test_id ").where("processed_tests.user_id = ? and tests.level = ?", self.id, level)
+  end
 end
