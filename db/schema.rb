f24c8b67d5e8408a661e826110c96cf7e35fc54d
JuicyFlash <tankedet@bk.ru>
2022-02-15 18:09:18 +0300

    миграция_модель пользователь-тест processed_test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_143358) do
+ActiveRecord::Schema.define(version: 2022_02_15_150513) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2022_02_15_143358) do
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "processed_tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_processed_tests_on_test_id"
+    t.index ["user_id"], name: "index_processed_tests_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -52,6 +61,8 @@ ActiveRecord::Schema.define(version: 2022_02_15_143358) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "processed_tests", "tests"
+  add_foreign_key "processed_tests", "users"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
 end
---
+++ test/models/processed_test_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ProcessedTestTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
