4696841a52c43d5c5b79d1b3ce963f5f6fd29e7c
JuicyFlash <tankedet@bk.ru>
2022-02-16 21:48:21 +0300

    валидации и scope методы

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,18 @@ class Question < ApplicationRecord
   has_many :answers
   belongs_to :test
 
+  validates :body, presence: true
+  validate :validate_answers_count
+
+  def correct_answer
+    answers.correct
+  end
+
+  private
+
+  def validate_answers_count
+    count = answers.count
+    errors.add :answers_count, 'wrong count' if count < 1 || count > 4
+  end
+
 end
