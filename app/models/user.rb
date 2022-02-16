db297e207fc1438a2bfdf70ea3c0ff02298ee5e4
JuicyFlash <tankedet@bk.ru>
2022-02-16 03:38:59 +0300

    ассоциации user - tests user - processed_tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+
+  has_many :processed_tests
+  has_many :tests, through: :processed_tests
 
   def get_tests_by_level(level)
     Test.joins("JOIN processed_tests on tests.id = processed_tests.test_id ").where("processed_tests.user_id = ? and tests.level = ?", self.id, level)
