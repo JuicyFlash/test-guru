b11c5382e3a48ab1745bc9f82f9acc577e14860c
JuicyFlash <tankedet@bk.ru>
2022-02-16 03:41:16 +0300

    накат актуальных миграций

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_150513) do
+ActiveRecord::Schema.define(version: 2022_02_16_000312) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2022_02_15_150513) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -65,4 +67,5 @@ ActiveRecord::Schema.define(version: 2022_02_15_150513) do
   add_foreign_key "processed_tests", "users"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
