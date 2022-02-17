d829d83a78f9f4591804617e194e07e4e32c93c5
JuicyFlash <tankedet@bk.ru>
2022-02-17 18:49:53 +0300

    валидация по количеству ответов переработана перенесена из Question в Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,6 +6,23 @@ class Answer < ApplicationRecord
 
   validates :body, presence: true
 
+
+
+  validate :validate_count_on_update, on: :update
+  validate :validate_count_on_create, on: :create
+
   scope :correct, -> { where(correct: :true) }
+
+  private
+
+  def validate_count_on_update
+    count = Answer.where('question_id = ?', question_id).count
+    errors.add(:answers_count, 'wrong count') if count > 4
+  end
+
+  def validate_count_on_create
+    count = Answer.where('question_id = ?', question_id).count
+    errors.add(:answers_count, 'wrong count') if count >= 4
+  end
 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -6,17 +6,8 @@ class Question < ApplicationRecord
   belongs_to :test
 
   validates :body, presence: true
-  validate :validate_answers_count
 
   def correct_answer
     answers.correct
   end
-
-  private
-
-  def validate_answers_count
-    count = answers.count
-    errors.add :answers_count, 'wrong count' if count < 1 || count > 4
-  end
-
 end
