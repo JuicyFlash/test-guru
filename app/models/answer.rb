ba77d7d6590704e9ee21c3df607ee7211dd7dcc2
JuicyFlash <tankedet@bk.ru>
2022-02-17 23:22:21 +0300

    переработал валидацию ассоциация вместо запроса

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,22 +6,14 @@ class Answer < ApplicationRecord
 
   validates :body, presence: true
 
-
-
-  validate :validate_count_on_update, on: :update
-  validate :validate_count_on_create, on: :create
+  validate :validate_count, on: :create
 
   scope :correct, -> { where(correct: :true) }
 
   private
 
-  def validate_count_on_update
-    count = Answer.where('question_id = ?', question_id).count
-    errors.add(:answers_count, 'wrong count') if count > 4
-  end
-
-  def validate_count_on_create
-    count = Answer.where('question_id = ?', question_id).count
+  def validate_count
+    count = question.answers.count
     errors.add(:answers_count, 'wrong count') if count >= 4
   end
 
