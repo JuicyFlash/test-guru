03da755e776eb8116a03a2aa886995c022cc9921
JuicyFlash <tankedet@bk.ru>
2022-03-25 21:58:46 +0300

    хелперы

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def current_year
+    "#{Time.current.year} год"
+  end
+
+  def github_url(author, repo)
+    link_to author, repo, target: '_blank'
+    #  "Автор #{author}. Проект <a target=_blank href=#{repo}>test-guru</a>."
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
-end
+  def question_header(action)
+    case action
+    when :create
+      "Create New #{@test.title} Question"
+    when :edit
+      "Edit #{@test.title} Question"
+    end
+  end
+  end
