b81aee44be34fb535b1d299fbf75091eb49a78dd
JuicyFlash <tankedet@bk.ru>
2022-03-28 18:13:49 +0300

    В question_header передаётся объект question

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,10 +1,9 @@
 module QuestionsHelper
-  def question_header(action)
-    case action
-    when :create
+  def question_header(question)
+    if question.persisted?
+      "Edit #{Test.find(question.test_id).title} Question"
+    else
       "Create New #{@test.title} Question"
-    when :edit
-      "Edit #{@test.title} Question"
     end
   end
   end
