012d3542467398390c12c3381e4c7e8de1822558
JuicyFlash <tankedet@bk.ru>
2022-03-28 18:12:09 +0300

    1. Список тестов - tests#index 2. Детализация теста (тест + список вопросов) - tests#show, рендерится с помощью фрагмента questions\_index

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,17 @@
 class TestsController < ApplicationController
+
+  before_action :find_test, only: %i[show]
+  before_action :all_test, only: %i[index]
+
+  def show; end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def all_test
+    @tests = Test.all
+  end
 end
