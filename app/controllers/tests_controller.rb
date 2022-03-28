2f769f37b4026b0c94ede8406b057cef9464d02b
JuicyFlash <tankedet@bk.ru>
2022-03-28 20:05:16 +0300

    Убрал колбэки и лишние методы

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,17 +1,11 @@
 class TestsController < ApplicationController
 
-  before_action :find_test, only: %i[show]
-  before_action :all_test, only: %i[index]
+  def index
+    @tests = Test.all
+  end
 
-  def show; end
-
-  private
-
-  def find_test
+  def show
     @test = Test.find(params[:id])
   end
 
-  def all_test
-    @tests = Test.all
-  end
 end
