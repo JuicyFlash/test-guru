15b503a5f02129850948e0c38bfa1c11a0735dc7
JuicyFlash <tankedet@bk.ru>
2022-04-07 21:25:19 +0300

    дублирующийся код убран в колбэк

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,18 +1,23 @@
 class TestsController < ApplicationController
 
+  before_action :find_test, only: %i[show start]
+
   def index
     @tests = Test.all
   end
 
-  def show
-    @test = Test.find(params[:id])
-  end
+  def show; end
 
   def start
-    @test = Test.find(params[:id])
     @user = User.first
     @user.tests.push(@test)
     redirect_to @user.processed_test(@test)
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
   end
 
 end
