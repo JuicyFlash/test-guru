60f928d2578fd6c58fc08316a17dd4c880c1a8c9
JuicyFlash <tankedet@bk.ru>
2022-04-07 21:27:18 +0300

    логика следующего вопроса перенесена из метода в колбэк логика определения корректности прохождения теста полностью перенесена в модель

--- app/models/processed_test.rb
+++ app/models/processed_test.rb
@@ -6,13 +6,12 @@ class ProcessedTest < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
   before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
 
   def accept!(answer_ids)
     if correct_answer? (answer_ids)
       self.correct_questions += 1
     end
-    self.question_number += 1 if test.questions.count < question_number
-    self.current_question = next_question
     save!
   end
 
@@ -20,12 +19,24 @@ class ProcessedTest < ApplicationRecord
     current_question.nil?
   end
 
+  def success?
+    percent_result >= percents_for_pass_test
+  end
+
+  def percent_for_pass_test
+    85
+  end
+
   def percent_result
-    #84
     (self.correct_questions.to_f / test.questions.count * 100).to_i
   end
 
   private
+
+  def before_validation_set_next_question
+    self.current_question = next_question
+    self.question_number += 1 if question_number < test.questions.count
+  end
 
   def before_validation_set_first_question
     self.current_question = self.test.questions.first if test.present?
