3389382312e76ad967f70f6eeb1b86743ac514ee
JuicyFlash <tankedet@bk.ru>
2022-04-14 23:43:18 +0300

    Добавлено новое поле password_digest в таблицу Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_223536) do
+ActiveRecord::Schema.define(version: 2022_04_11_105304) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2022_04_05_223536) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
