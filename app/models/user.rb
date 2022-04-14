fb02eb99b1600cd0f517531a7c3857cfabf59ba5
JuicyFlash <tankedet@bk.ru>
2022-04-14 23:48:40 +0300

    Реализован ресурс User  - Форма создания нового пользователя  - В сборку добавлен гем bcrypt  - Изменения в модели (валидация логина формат и уникальность и работа с паролем)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
-
+require 'digest/sha1'
 class User < ApplicationRecord
 
   has_many :processed_tests
   has_many :tests, through: :processed_tests
 
-  validates :email, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Incorrect email format", on: :create }
+  validates :email, uniqueness: { message: "Email already in use" }
+
+  has_secure_password
+
+  def get_tests_by_level(level)
+    Test.joins("JOIN processed_tests on tests.id = processed_tests.test_id ").where("processed_tests.user_id = ? and tests.level = ?", self.id, level)
+  end
 
   def processed_test(test)
     processed_tests.order(id: :desc).find_by(test_id: test.id)
   end
 
-  def get_tests_by_level(level)
-    Test.joins("JOIN processed_tests on tests.id = processed_tests.test_id ").where("processed_tests.user_id = ? and tests.level = ?", self.id, level)
-  end
 end
