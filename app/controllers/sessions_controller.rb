dc438166e5d2f3289dd830d1ddc72046b8bc8ba3
JuicyFlash <tankedet@bk.ru>
2022-04-14 23:55:03 +0300

    Реализация ресурса session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new; end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to cookies.delete(:origin_path) || tests_path
+    else
+      flash.now[:alert] = 'Verify your Email and Password'
+      render :new
+    end
+
+  end
+end
