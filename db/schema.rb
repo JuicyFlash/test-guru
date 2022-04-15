428cf938af63d0843392da8b9696bb047f511440
JuicyFlash <tankedet@bk.ru>
2022-04-15 22:01:06 +0300

    Ограничение уникальности email на уровне БД

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_105304) do
+ActiveRecord::Schema.define(version: 2022_04_15_185812) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2022_04_11_105304) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
