60605ffb6f849ac2ab1cd82078868e08d0d4469b
JuicyFlash <tankedet@bk.ru>
2022-08-19 20:21:00 +0300

    Настройки почты изменены на gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    address: 'smtp.mail.ru',
+    address: 'smtp.gmail.com',
     port: 465,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
