5c10c413dc34199f1741a0b1124ba1e97ab343f5
JuicyFlash <tankedet@bk.ru>
2022-11-01 01:45:56 +0300

    Правка правила все тесты категории.

--- app/services/finish_tests_by_category_rule_specification.rb
+++ app/services/finish_tests_by_category_rule_specification.rb
@@ -11,7 +11,7 @@ class FinishTestsByCategoryRuleSpecification < AbstractRuleSpecification
     tests = Test.where("category_id=? and ready=true", category.id).order('tests.id DESC').pluck(:id)
     user_tests = []
     ProcessedTest.joins("join tests on tests.id=processed_tests.test_id").
-                  where("processed_tests.user_id=? and tests.category_id=? and tests.ready=true" , @processed_test.user_id, @value).
+                  where("processed_tests.user_id=? and tests.category_id=? and tests.ready=true" , @processed_test.user_id, category.id).
                   order('processed_tests.test_id DESC').each  do |processed_test|
       user_tests << processed_test.test_id if processed_test.success?
     end
