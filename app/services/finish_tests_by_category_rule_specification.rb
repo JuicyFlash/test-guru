b2606059c01de180a4fccda8d1b8418da7123705
JuicyFlash <tankedet@bk.ru>
2022-11-01 01:14:15 +0300

    Изменение правила (поиск по имени категории)

--- app/services/finish_tests_by_category_rule_specification.rb
+++ app/services/finish_tests_by_category_rule_specification.rb
@@ -5,7 +5,7 @@ class FinishTestsByCategoryRuleSpecification < AbstractRuleSpecification
   end
 
   def satisfies?
-    tests = Test.where("category_id=? and ready=true", @value).order('tests.id DESC').pluck(:id)
+    tests = Test.where("category_id=? and ready=true", Category.where(title:@value)).order('tests.id DESC').pluck(:id)
     user_tests = []
     ProcessedTest.joins("join tests on tests.id=processed_tests.test_id").
                   where("processed_tests.user_id=? and tests.category_id=? and tests.ready=true" , @processed_test.user_id, @value).
